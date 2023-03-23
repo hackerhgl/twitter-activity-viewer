b41368b20a7fd43c2566615009c615909ccde75c
hackerhgl <hackerhgl@gmail.com>
2023-03-23 11:00:16 +0500

    Client setup
    - Using VueJS
    - TailwindCSS
    - Importing data

---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import index from '../../../tweets_index.json';
+import users from '../../../users.json';
+
+
+console.log(index)
+console.log(users)
+</script>
+
+<template>
+    <div class="mx-auto md:max-w-3xl px-4 sm:px-6 lg:px-0">
+        <h1 class="text-2xl text-blue-500">Testing-y</h1>
+    </div>
+</template>
