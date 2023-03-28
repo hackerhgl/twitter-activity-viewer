26002be0e9854e53638e09b5f546b78d23b0d3f7
hackerhgl <hackerhgl@gmail.com>
2023-03-28 09:30:02 +0500

    Rendering list of tweets

--- client/src/components/FilterUserItem.vue
+++ client/src/components/FilterUserItem.vue
@@ -1,22 +1,36 @@
 <script lang="ts">
-// import type { TwitterUser } from '@/types/user';
+import type { TwitterUser } from '@/types/user';
+import type { PropType } from 'vue';
 
-export default {};
-
-// export default {
-//     name: 'UserItem',
-//     props: {
-//         user: {
-//             type: TwitterUser,
-//             required: true
-//         }
-//     }
-// }
+export default {
+    name: 'UserItem',
+    props: {
+        user: {
+            type: Object as PropType<TwitterUser>,
+            required: true
+        },
+        count: {
+            type: Object as PropType<number>,
+            required: true
+        }
+    }
+}
 </script>
 
 <template>
-    <div  class="px-2 py-1 mx-1 border-zinc-800 border-2">
-        <!-- {{ user.name }} -->
-        USER
-    </div>>
+    <div class="px-4 py-2 mx-1 border-zinc-800 border-2 min-w-max">
+        <div class="flex flex-row">
+            <div class="w-20 h-20">
+                <img :src="user.profile_image_url_https" :alt="user.name" class="object-contain w-16 h-16 rounded-full" />
+            </div>
+            <div class="min-w-0">
+                <div class="text-sm text-zinc-500">{{ user.name }}</div>
+                <div class="text-sm text-zinc-500">@{{ user.screen_name }}</div>
+                <div class="text-sm text-zinc-500">
+                    <span>Actions:</span>
+                    @{{ count }}
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,12 +2,14 @@
 import UserItem  from '@/components/FilterUserItem.vue';
 import indexes from '@/assets/tweets_index.json';
 import users from '@/assets/users.json';
+import tweets from '@/assets/data.json';
 import type { TwitterUser } from '@/types/user';
+import type { Tweet } from '@/types/tweet';
 
-// function getUser(userId: string) {
-//     const user = (users as any)[userId] as unknown as any;
-//     return user as TwitterUser;
-// }
+function getUser(userId: string) {
+    const user = (users as any)[userId];
+    return user as TwitterUser;
+}
 
 </script>
 
@@ -20,19 +22,18 @@ import type { TwitterUser } from '@/types/user';
             </div>
         </div>
         <div id="filters">
-            <h2>Select users</h2>
-            <div class="flex flex-row overflow-auto">
+            <h2 class="text-xl">Users filters</h2>
+            <div class="mt-2" />
+            <div class="flex flex-row flex-nowrap overflow-auto w-full">
                 <template v-for="index in indexes" :key="index.user">
-
-                    <div class="flex flex-col">
-                        <div>{{ JSON.stringify(index) }}</div>
-                        <UserItem />
-                        <template>
-                        </template>
-                    </div>
+                    <UserItem :user="getUser(index.user)" :count="index.count" />
                 </template>
             </div>
         </div>
-
+        <template v-for="tweet in tweets" :key="tweet.id_str">
+            <div>
+                {{ tweet.text }}
+            </div>
+        </template>
     </div>
 </template>
